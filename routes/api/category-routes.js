9e76a05127e8d015488d608a8bbd26d56f1912a0
wasim202 <65715473+wasim202@users.noreply.github.com>
2022-09-13 21:25:25 -0600

    start challenge

---
+++ routes/api/category-routes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try{
+  const categoryData = await Category.findAll();
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
+});
+
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try{
+    const categoryData = await Category.findByPk(req.params.id, {
+        include: [{model: Product} ]
+    });
+    if (!categoryData) {
+        res.status(404).json({ message: 'No location found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(locationData);
+  }catch(err){
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
